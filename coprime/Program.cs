876e82f0d6c348b3752fc78dbe919856d278de92
Rahul <Rahul@DESKTOP-C2D05LV>
2022-10-07 14:28:51 -0700

    Add project files.

---
+++ coprime/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace coprime
+{
+  
+    public  class Program
+    {
+        public  void  isCoprime(int a, int b)
+        {
+            int c = 0;
+            for (int i = 1; i <= a && i <= b; i++)
+            {
+                if (a % i == 0 && b % i == 0)
+                    c++;
+            }
+
+            if (c == 1)
+            {
+                Console.WriteLine("Coprime");
+            }
+            else
+            {
+                Console.WriteLine("not coprime");
+            }
+        }
+
+       
+        
+
+            static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.isCoprime(8,15);
+        }
+    }
+}
